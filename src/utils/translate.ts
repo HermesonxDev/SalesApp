6f3c73ddef994105e58b7170a5764bb28b93ecf0
HermesonxBerp <hermesonsantos@berp.com.br>
2025-06-05 13:15:36 -0300

    new translates

--- src/utils/translate.ts
+++ src/utils/translate.ts
@@ -1,4 +1,5 @@
 const translations = {
+    // Cliente
     'Customer created has successfully!': 'Cliente criado com sucesso!',
     'Error on create customer.': 'Erro ao criar cliente.',
 
@@ -6,7 +7,17 @@ const translations = {
     'Error on editing customer.': 'Erro ao editar cliente.',
 
     'Customer deleted has successfully!': 'Cliente deletado com sucesso!',
-    'Error on deleting customer.': 'Erro ao deletar cliente.'
+    'Error on deleting customer.': 'Erro ao deletar cliente.',
+
+    // Produto
+    'Product created has successfully!': 'Produto criado com sucesso!',
+    'Error on product creation.': 'Erro ao criar produto.',
+
+    'Product edited has successfully!': 'Produto editado com sucesso!',
+    'Error on editing product.': 'Erro ao editar produto.',
+
+    'Product deleted has successfully!': 'Produto deletado com sucesso!',
+    'Error on deleting product.': 'Erro ao deletar produto'
 } as const
 
 export type MessageKey = keyof typeof translations;
